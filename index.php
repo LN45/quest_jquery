8830b964570601fa35016d3c0e55304852f42611
Helene Bloch <ln-45@live.fr>
2018-10-21 18:19:52 +0200

    quete jquery

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+    <script type="text/javascript" src="jquery/demo.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="slides">
+            <ul>
+                <li><a href="images/image1.jpg"><img src="images/image1.jpg" alt="cats" width="350" height="200" /></a></li>
+                <li><a href="images/image2.jpeg"><img src="images/image2.jpeg" alt="cats" width="350" height="200" /></a></li>
+                <li><a href="images/image3.jpeg"><img src="images/image3.jpeg" alt="cats" width="350" height="200" /></a></li>
+                <li><a href="images/image4.jpeg"><img src="images/image4.jpeg" alt="cats" width="350" height="200" /></a></li>
+            </ul>
+            <button id="next" class="ui-icon ui-icon-caret-1-e">❯</button>
+            <button id="prev" class="ui-icon ui-icon-caret-1-w">❮</button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ jquery/demo.js
@@ -0,0 +1,8 @@
+$(function() {
+    setInterval(function () {
+        $(".slides ul").animate({marginLeft: -350}, 800, function () {
+            $(this).css({marginLeft: 0}).find("li:last").after($(this).find("li:first"));
+        })
+    }, 3500);
+});
+;
